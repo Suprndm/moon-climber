4337f7d7481280d4dbb761340899f3c33cb4a5c8
Keitaro92 <nicolas.dalmolin@gmail.com>
2018-05-26 20:50:11 +0200

    initial commit

---
+++ MoonClimber/MoonClimber/App.xaml.cs
@@ -0,0 +1,33 @@
+using System;
+using Odin;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace MoonClimber
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new OdinPage();
+        }
+
+        protected override void OnStart()
+        {
+            ((OdinPage)MainPage).OnStart<Game>();
+        }
+
+        protected override void OnSleep()
+        {
+            ((OdinPage)MainPage).OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            ((OdinPage)MainPage).OnResume();
+        }
+    }
+}
